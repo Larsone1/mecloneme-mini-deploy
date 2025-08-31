78b5f66c1c560b6764d79761963f045c29d8e55a
Larsone1 <maciej.miekczynski@gmail.com>
2025-08-31 13:30:30 +0200

    Add files via upload

--- MeCloneMe_min_deploy/backend/static/js/force_refresh.js
+++ MeCloneMe_min_deploy/backend/static/js/force_refresh.js
@@ -1,16 +1,37 @@
-// mcm hotfix mcm-20250831-110605-utc
-// Open /static/js/force_refresh.js directly to nuke caches & SW on this device.
-(async () => {
-  if ('serviceWorker' in navigator) {
-    try {
-      const regs = await navigator.serviceWorker.getRegistrations();
-      for (const r of regs) try { await r.unregister(); } catch(_){}
-    } catch(_) {}
-  }
-  if ('caches' in window) {
-    const keys = await caches.keys();
-    await Promise.all(keys.map(k => caches.delete(k)));
-  }
-  // Hard reload
-  location.replace(location.pathname + '?bust=mcm-20250831-110605-utc');
+// mcm cache/ServiceWorker hotfix (gated)
+// Use by visiting your app URL with ?nuke=1 OR by opening /static/diag/index.html and pressing the button.
+// Safe to include globally – it does NOTHING unless ?nuke=1 is present on the current page.
+(() => {
+  try {
+    const url = new URL(location.href);
+    const shouldNuke = url.searchParams.has('nuke');
+    if (!shouldNuke) {
+      // No-op in normal navigation
+      return;
+    }
+    (async () => {
+      try {
+        if ('serviceWorker' in navigator) {
+          try {
+            const regs = await navigator.serviceWorker.getRegistrations();
+            await Promise.all(regs.map(r => r.unregister().catch(() => {})));
+          } catch (_) {}
+        }
+        if ('caches' in window) {
+          try {
+            const keys = await caches.keys();
+            await Promise.all(keys.map(k => caches.delete(k).catch(() => {})));
+          } catch (_) {}
+        }
+      } finally {
+        try {
+          url.searchParams.delete('nuke');
+          url.searchParams.set('bust', 'mcm-' + Date.now());
+          location.replace(url.pathname + (url.searchParams.toString() ? ('?' + url.searchParams.toString()) : ''));
+        } catch (_) {
+          location.reload();
+        }
+      }
+    })();
+  } catch (_) {}
 })();
